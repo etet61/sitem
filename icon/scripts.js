946a7f361f5d8b9c6228c4051f021c043a3e85e5
etet61 <enesturgut6161@gmail.com>
2024-06-08 11:22:02 +0300

    ilk oluşturma

---
+++ icon/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Ana sayfadaki butona tıklandığında bir mesaj göster
+    document.getElementById('welcomeButton').addEventListener('click', function () {
+        alert('Hoş Geldiniz! Bu benim kişisel web sitesi.');
+    });
+});
